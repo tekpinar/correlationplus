550b291c8c9a4926f7c57a9dfeeb516dfb70dd24
Bertrand <bneron@pasteur.fr>
2020-08-05 15:37:21 +0200

    :sparkles: update setup.py
    add entrypoint
    use correlation_plus.__version__ for version,
    use requiremenets.txt
    ...

--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
-#from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 
-#This line was after license. 
-##packages=['correlationPlus'],
-setup(name='correlationPlus', 
-      package_dir = {'correlationPlus': 'src'},
+from correlation_plus import __version__ as cp_vers
+
+setup(name='correlationPlus',
+      version=cp_vers,
+      description=" Python module to plot dynamical correlations maps for proteins.",
+      long_description=open('README').read(),
+      author="Mustafa Tekpinar",
+      author_email="tekpinar@buffalo.edu",
+      url="",
+      download_url='',
+      license="MIT License",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Bio-Informatics'
+          ],
+      python_requires='>=3.6',
+      install_requires=open("requirements.txt").read().split(),
+      # zip_safe=False,
+      packages=[p for p in find_packages() if p != 'tests'],
+      # file where some variables must be fixed by install
       entry_points={
-        'console_scripts': [
-            'correlationPlus = src.correlationPlus:main',
-        ],
-      },
-#      scripts=['src/correlationPlus.py'],
-      version='0.0.5', 
-      description='A Python module to plot dynamical correlations maps for proteins.',
-      author='Mustafa Tekpinar',
-      author_email='tekpinar@buffalo.edu',
-      license='MIT License',
-      url='https://github.com/tekpinar/correlationPlus',
-      install_requires=['numpy', 'prody', 'matplotlib'],
+          'console_scripts': [
+              'mapAnalysis=correlation_plus.scripts.mapAnalysis:mapAnalysisApp'
+          ]
+      }
       )
-
