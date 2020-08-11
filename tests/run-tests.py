4588e5d9f3aa0810dcc21dbdeac23d09e53a5ab9
Bertrand <bneron@pasteur.fr>
2020-08-11 13:47:29 +0200

    fix path to work with new project structure

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -1,3 +1,28 @@
+###############################################################################
+# correlationPlus - Python module to plot dynamical correlations maps         #
+#                   for proteins.                                             #
+# Authors: Mustafa Tekpinar                                                   #
+# Copyright Mustafa Tekpinar 2017-2018                                        #
+# Copyright CNRS-UMR3528, 2019                                                #
+# Copyright Institut Pasteur Paris, 2020                                       #
+#                                                                             #
+# This file is part of correlationPlus.                                       #
+#                                                                             #
+# correlationPlus is free software: you can redistribute it and/or modify     #
+# it under the terms of the GNU Lesser General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# correlationPlus is distributed in the hope that it will be useful,          #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU LESSER General Public License for more details.                         #
+#                                                                             #
+# You should have received a copy of the GNU Lesser General Public License    #
+# along with correlationPlus.  If not, see <https://www.gnu.org/licenses/>.   #
+###############################################################################
+
+
 import os
 
 """
@@ -9,9 +34,10 @@ So, I will do it here by checking diffs of output images.
 
 def runTests():
     # Test correlationMapApp for nDCC maps
-    os.system("python ../src/correlationPlus.py mapAnalysisApp "
-              "-i ../examples/6fl9_just_prot_anm_100_modes_rc_15_cross-correlations.txt "
-              "-p ../examples/6fl9_centeredOrientedAligned2Z.pdb "
+    prefix = os.path.normpath(os.path.join(__file__, '..', '..'))
+    os.system(f"python {prefix}/correlation_plus/scripts/correlationPlus.py mapAnalysis "
+              f"-i {prefix}/examples/6fl9_just_prot_anm_100_modes_rc_15_cross-correlations.txt "
+              f"-p {prefix}/examples/6fl9_centeredOrientedAligned2Z.pdb "
               "-s absdcc")
 
     # Test correlationMapApp for absolute nDCC maps
@@ -21,13 +47,18 @@ def runTests():
     # os.system()
 
     # Test diffMapApp for LMI maps
-    os.system("python ../src/diffMap.py diffMapApp "
-              "-i ../examples/6fl9_rc15_scalCoeff1_100_modes_lmi_v2.dat "
-              "-j ../examples/zacharias_rc15_scalCoeff15_100_modes_lmi.dat "
-              "-p ../examples/6fl9_centeredOrientedAligned2Z.pdb "
+    os.system(f"python {prefix}/correlation_plus/scripts/correlationPlus.py diffMap "
+              f"-i {prefix}/examples/6fl9_rc15_scalCoeff1_100_modes_lmi_v2.dat "
+              f"-j {prefix}/examples/zacharias_rc15_scalCoeff15_100_modes_lmi.dat "
+              f"-p {prefix}/examples/6fl9_centeredOrientedAligned2Z.pdb "
               "-t lmi")
 
 
 if __name__ == "__main__":
+    import sys
+    COR_PLUS_HOME = os.path.abspath(os.path.join(__file__, '..', '..'))
+    old_path = sys.path
+    if COR_PLUS_HOME not in sys.path:
+        sys.path.insert(0, COR_PLUS_HOME)
     runTests()
-
+    sys.path = old_path
